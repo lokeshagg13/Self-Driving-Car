682faabdac5b30ab9018be9e915bd260da1d6f5b
LOKESH <32417590+lokeshagg13@users.noreply.github.com>
2024-12-20 22:19:55 +0530

    Basic car controls

---
+++ controls.js
@@ -0,0 +1,46 @@
+class Controls {
+    constructor() {
+        this.forward = false;
+        this.left = false;
+        this.right = false;
+        this.reverse = false;
+
+        this.#addKeyboardListeners();
+    }
+
+    #addKeyboardListeners() {
+        document.onkeydown = (ev) => {
+            switch (ev.key) {
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
+            }
+        }
+
+        document.onkeyup = (ev) => {
+            switch (ev.key) {
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
+            }
+        }
+    }
+}
